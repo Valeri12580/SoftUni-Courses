1885fa1bc0dea74959b430be01e4a1d75c2bc87a
Valeri <valeri125we@gmail.com>
2019-06-25 15:58:32 +0300

    added exercises

---
+++ Java Advanced/Java Advanced/Generics- Lab/src/GenericScale/GenericScale.java
@@ -0,0 +1,15 @@
+package GenericScale;
+
+public class GenericScale<T> {
+    private T firstElement;
+    private T secondElement;
+
+    public GenericScale(T firstElement, T secondElement) {
+        this.firstElement = firstElement;
+        this.secondElement = secondElement;
+    }
+
+
+
+
+}
