7916e75c67578526078e18c26ab3b7dc748e9bf3
Valeri <valeri125we@gmail.com>
2019-08-29 16:48:42 +0300

    exercise

---
+++ Java Advanced/Java OOP/Prep Exam april 18/src/Main.java
@@ -0,0 +1,13 @@
+import core.ManagerControllerImpl;
+import core.interfaces.ManagerController;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
