b2a33dc2d7d9d8c003387adeab68629b735a7673
Valeri <valeri125we@gmail.com>
2020-04-30 10:47:42 +0300

    del

--- Java Web sept 2019/Java Web Basics/Workshop-JSF+JPA/src/main/java/repositories/interfaces/BaseRepository.java
+++
@@ -1,8 +0,0 @@
-package repositories.interfaces;
-
-public interface BaseRepository <T,ID>{
-
-    void save(T entity);
-
-    void delete(ID id);
-}
--- Java Web sept 2019/Java Web Basics/Workshop-JSF+JPA/src/main/java/repositories/interfaces/JobRepository.java
+++
@@ -1,6 +0,0 @@
-package repositories.interfaces;
-
-import domain.entities.Job;
-
-public interface JobRepository extends BaseRepository<Job,String> {
-}
