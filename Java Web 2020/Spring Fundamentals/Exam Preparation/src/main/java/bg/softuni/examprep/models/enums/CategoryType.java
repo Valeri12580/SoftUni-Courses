3709abbb89e803a4a5811633ab8855bfb883bbc5
Valeri <valeri125we@gmail.com>
2020-06-22 22:16:17 +0300

    up

--- Java Web 2020/Spring Fundamentals/Exam Preparation/src/main/java/bg/softuni/examprep/models/enums/CategoryType.java
+++
@@ -1,5 +0,0 @@
-package bg.softuni.examprep.models.enums;
-
-public enum CategoryType {
-    SHIRT,DENIM,SHORTS,JACKET
-}
--- Java Web 2020/Spring Fundamentals/Exam Preparation/src/main/java/bg/softuni/examprep/models/enums/Gender.java
+++
@@ -1,5 +0,0 @@
-package bg.softuni.examprep.models.enums;
-
-public enum Gender {
-    MALE,FEMALE
-}
